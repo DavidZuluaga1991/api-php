d3437decb42a5a7c4319cdd4c36e9258dd9d6410
DavidZuluaga1991 <davidzuluaga1991@gmail.com>
2019-04-02 19:57:54 -0500

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ Website: https://www.allphptricks.com
 
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","","allphptricks");
+$con = mysqli_connect("10.128.62.82","davidzuluaga","davidzuluaga","allphptricks");
 	if (mysqli_connect_errno()){
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		die();
